10ae376738f03e94cee58dff3d9273c9050b7c2e
MBacioglu <302145958@student.rocmondriaan.nl>
2024-06-29 22:57:07 +0200

    basis game is af maar ik ga nog wat verbeteren

--- js/game.js
+++ js/game.js
@@ -49,4 +49,55 @@ class GameScene extends Phaser.Scene {
         });
     }
 
+    update() {
+        if (this.cursors.left.isDown) {
+            this.car.setAngularVelocity(-150);
+        } else if (this.cursors.right.isDown) {
+            this.car.setAngularVelocity(150);
+        } else {
+            this.car.setAngularVelocity(0);
+        }
+
+        if (this.cursors.up.isDown) {
+            this.physics.velocityFromRotation(this.car.rotation, 200, this.car.body.velocity);
+        } else {
+            this.car.setVelocity(0);
+        }
+    }
 }
+
+
+// Pause Menu Scene
+class PauseMenuScene extends Phaser.Scene {
+    constructor() {
+        super({ key: 'PauseMenuScene' });
+    }
+
+    create() {
+        this.add.text(400, 300, 'Paused\nPress R to Resume', { fontSize: '32px', fill: '#FFF', align: 'center' }).setOrigin(0.5);
+        this.input.keyboard.on('keydown-R', () => {
+            this.scene.stop();
+            this.scene.resume('GameScene');
+        });
+    }
+}
+
+// Game configuration
+const config = {
+    type: Phaser.AUTO,
+    width: 900,
+    height: 600,
+    parent: 'game-container',
+    scene: [MainMenuScene, GameScene, PauseMenuScene],
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
+    }
+};
+
+
+// Initialize Phaser game
+const game = new Phaser.Game(config);
